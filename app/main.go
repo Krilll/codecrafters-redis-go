c6337778fd772df96aa3933781163c126b701054
Lerti <rik.zh@mail.ru>
2025-08-28 11:44:43 +0300

    add goroutine for listener

--- app/main.go
+++ app/main.go
@@ -14,21 +14,23 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
-	//
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	listener, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
 
 	for {
-		conn, err := l.Accept()
+		conn, err := listener.Accept()
 		if err != nil {
 			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
-		conn.Write([]byte("+PONG\r\n"))
+		go ping(conn)
 	}
+}
 
+func ping(conn net.Conn) {
+	defer conn.Close()
+	conn.Write([]byte("+PONG\r\n"))
 }
