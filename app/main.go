a53ecb50aa3f1b4431151b07f185a57e1b381559
Lerti <rik.zh@mail.ru>
2025-08-28 11:51:35 +0300

    another for move

--- app/main.go
+++ app/main.go
@@ -14,23 +14,18 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	for {
-		listener, err := net.Listen("tcp", "0.0.0.0:6379")
-		if err != nil {
-			fmt.Println("Failed to bind to port 6379")
-			os.Exit(1)
-		}
-
-		conn, err := listener.Accept()
-		if err != nil {
-			fmt.Println("Error accepting connection: ", err.Error())
-			os.Exit(1)
-		}
-		go ping(conn)
+	listener, err := net.Listen("tcp", "0.0.0.0:6379")
+	if err != nil {
+		fmt.Println("Failed to bind to port 6379")
+		os.Exit(1)
 	}
-}
 
-func ping(conn net.Conn) {
-	defer conn.Close()
-	conn.Write([]byte("+PONG\r\n"))
+	conn, err := listener.Accept()
+	if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+	for {
+		conn.Write([]byte("+PONG\r\n"))
+	}
 }
